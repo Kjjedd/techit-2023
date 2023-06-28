0984b809998188b6659b189f2bd6a89a529ea8ff
Kjjedd <vocolate17@gmail.com>
2023-06-28 12:41:42 +0900

    Console IO

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -1,2 +1,19 @@
+import java.util.*;
 public class ConsoleIO {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Enter the name.");
+        System.out.print(">> ");
+        String name = sc1.nextLine();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Enter the Birthday.(YYYYMMDD)");
+        System.out.print(">> ");
+        String birth = sc2.nextLine();
+        StringBuffer buf = new StringBuffer(birth);
+        buf.insert(4, "-");
+        buf.insert(7, "-");
+
+        System.out.printf("NAME: %s\nBIRTHDAY: %s", name, buf);
+    }
 }
